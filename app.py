9cb1f417e915ac61f3a8fc20bd50f99e7099e8ae
Moses <moses.junior@student.moringaschool.com>
2021-09-20 12:50:35 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,jsonify
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Lunch break',
+        'description': u'Having a break to take lunch for 10 minutes', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+if __name__ == '__main__':
+    app.run(debug=True)
